108921476238d5fe9a2879ccb10777978b57db38
LeftTry <lerest.go@gmail.com>
2023-10-03 01:54:36 +0600

    Started to do Search Screen

--- app/src/main/java/com/example/shebetar/SearchScreen/SearchScreen.kt
+++ app/src/main/java/com/example/shebetar/SearchScreen/SearchScreen.kt
@@ -3,30 +3,56 @@ package com.example.shebetar.SearchScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen() {
+    val searchText = remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
+            .background(MaterialTheme.colorScheme.background),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Icon(
-            imageVector = Icons.Default.Search,
-            contentDescription = "search",
-            tint = Color(0xFF0F9D58)
-        )
-        Text(text = "Search", color = Color.Black)
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+        ){
+            TextField(value = searchText.value, onValueChange = {searchTextNew -> searchText.value = searchTextNew}, placeholder = {
+                Text(text = "Search something"
+                )}, shape = CircleShape, modifier = Modifier.padding(10.dp))
+            IconButton(onClick = { /*TODO*/ }, modifier = Modifier.background(MaterialTheme.colorScheme.primary)) {
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = "Search button",
+                    tint = MaterialTheme.colorScheme.surface
+                )
+            }
+        }
+
+
     }
 }
