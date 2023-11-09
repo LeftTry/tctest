a4891e74d7f5f4775cbd951d6ba3f1580d42cf8c
LeftTry <lerest.go@gmail.com>
2023-11-09 19:44:59 +0600

    Avatar is now generating + upgraded design

--- app/src/main/java/com/example/shebetar/ProfileScreen/ProfileScreen.kt
+++ app/src/main/java/com/example/shebetar/ProfileScreen/ProfileScreen.kt
@@ -2,21 +2,26 @@ package com.example.shebetar.ProfileScreen
 
 import android.content.Context
 import android.util.Log
+import androidx.annotation.ColorInt
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,17 +29,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
 import com.example.shebetar.Classes.User.User
 import com.example.shebetar.DataBase.getUserById
 import com.example.shebetar.DataBase.readUserDataFromJson
 import com.example.shebetar.R
+import com.example.shebetar.TopNavBar.TopNavBar
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.math.absoluteValue
 
 @Composable
 fun ProfileScreen(context: Context){
@@ -96,81 +106,104 @@ fun ProfileScreen(context: Context){
     }
 
      */
-    TwitterProfileScreen()
+    TwitterProfileScreen(user)
 }
 
 @Composable
-fun ProfileScreen() {
+fun ProfileScreen(user: User) {
     val bio by remember { mutableStateOf("Your bio goes here.") }
+    val color = Color("${user.id} / ${user.nickname}".toHslColor())
+    val initials = (user.nickname.take(1)).uppercase()
+    val avatarStyle = MaterialTheme.typography.headlineLarge
+    val avatarHeight = 64.dp
+    val bannerHeight = 150.dp
+    val horizontalPadding = 16.dp
+    Box (modifier = Modifier.fillMaxSize()){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.basicavatar), // Replace with your profile image
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(bannerHeight),
+                contentScale = ContentScale.Crop
+            )
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.basicavatar), // Replace with your profile image
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp),
-            contentScale = ContentScale.Crop
-        )
+            Spacer(modifier = Modifier.height(avatarHeight))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = user.firstName,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = horizontalPadding)
+            )
 
-        Text(
-            text = "Your Name",
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
+            Text(
+                text = "@" + user.nickname,
+                style = MaterialTheme.typography.headlineSmall,
+                color = Color.Blue,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
 
-        Text(
-            text = "@yourhandle",
-            style = MaterialTheme.typography.headlineMedium,
-            color = Color.Blue,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
+            Spacer(modifier = Modifier.height(8.dp))
 
-        Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = bio,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(horizontal = horizontalPadding)
+            )
 
-        Text(
-            text = bio,
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            ProfileActions(user)
 
-        ProfileActions()
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Divider(
+                color = Color.Gray,
+                thickness = 0.5.dp,
+                modifier = Modifier.padding(horizontal = horizontalPadding)
+            )
 
-        Divider(
-            color = Color.Gray,
-            thickness = 0.5.dp,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
-
-        TweetList()
+            TweetList(user)
+        }
+        Box(
+            Modifier
+                .size(avatarHeight)
+                .offset(x = horizontalPadding, y = bannerHeight - avatarHeight / 2),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = ColorPainter(color),
+                modifier = Modifier.clip(CircleShape),
+                contentDescription = "Avatar"
+            )
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawCircle(SolidColor(color))
+            }
+            Text(text = initials, style = avatarStyle, color = Color.White)
+        }
     }
 }
 
 @Composable
-fun ProfileActions() {
+fun ProfileActions(user: User) {
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text("123 Following", modifier = Modifier.padding(16.dp))
-        Text("456 Followers", modifier = Modifier.padding(16.dp))
+        Text(user.followingQuantity.toString(), modifier = Modifier.padding(16.dp))
+        Text(user.followersQuantity.toString(), modifier = Modifier.padding(16.dp))
     }
 }
 
 @Composable
-fun TweetList() {
+fun TweetList(user: User) {
     LazyColumn {
-        items(10) { index ->
+        items(user.posts.size) { index ->
             TweetCard()
         }
     }
@@ -201,20 +234,26 @@ fun TweetCard() {
 }
 
 @Composable
-fun TwitterProfileScreen() {
+fun TwitterProfileScreen(user: User) {
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text(text = stringResource(R.string.app_name)) },
-                backgroundColor = MaterialTheme.colorScheme.primary,
-                contentColor = Color.White,
-            )
+            TopNavBar()
         },
         content = {
             Column (modifier = Modifier.padding(it)){
-                ProfileScreen()
+                ProfileScreen(user)
             }
 
         }
     )
+}
+
+@ColorInt
+fun String.toHslColor(
+    saturation: Float = 0.5f,
+    lightness: Float = 0.4f
+): Int {
+    val hashCode = fold(0) { acc, char -> char.code + acc * 37 }
+    val hue = (hashCode % 360).absoluteValue.toFloat()
+    return ColorUtils.HSLToColor(floatArrayOf(hue, saturation, lightness))
 }
